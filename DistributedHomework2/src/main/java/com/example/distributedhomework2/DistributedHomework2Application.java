a42ebc31f696f5aae89baa32d9c2734723a4369d
cynLogin <144438294+cynLogin@users.noreply.github.com>
2023-11-13 20:36:46 +0800

    Add files via upload

---
+++ DistributedHomework2/src/main/java/com/example/distributedhomework2/DistributedHomework2Application.java
@@ -0,0 +1,13 @@
+package com.example.distributedhomework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedHomework2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedHomework2Application.class, args);
+    }
+
+}
