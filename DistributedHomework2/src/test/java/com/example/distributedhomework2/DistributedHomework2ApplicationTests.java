5dd623095b11230d57e917dbf5149e10e50f58ee
cynLogin <144438294+cynLogin@users.noreply.github.com>
2023-11-13 20:03:39 +0800

    Add files via upload

---
+++ DistributedHomework2/src/test/java/com/example/distributedhomework2/DistributedHomework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributedhomework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedHomework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
