cae93d3c4ba07375ca2b83019d0122eda6dac3c7
blendercc <1289108236@qq.com>
2023-11-30 10:29:31 +0800

    添加了bean和mapper

---
+++ DistributedHomework2/src/main/java/com/example/distributedhomework2/bean/CustomerBean.java
@@ -0,0 +1,5 @@
+package com.example.distributedhomework2.bean;
+
+public class CustomerBean {
+
+}
---
+++ DistributedHomework2/src/main/java/com/example/distributedhomework2/mapper/AdministratorMapper.java
@@ -0,0 +1,4 @@
+package com.example.distributedhomework2.mapper;
+
+public interface AdministratorMapper {
+}
